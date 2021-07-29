9ce6ed2afd17b0d4cb194425f414aabca2a60724
Vasav-Trehan <67497869+Vasav-Trehan@users.noreply.github.com>
2021-07-29 09:28:31 +0530

    Update profitcalculator.py

--- Profit Calulator/profitcalculator.py
+++ Profit Calulator/profitcalculator.py
@@ -1,1 +1,8 @@
+def sales_profit(sp , cp , q):
+    formula = str((sp - cp) * q)
+    total_cp = str(q * cp)
+    total_sp = str(q * sp)
+    print('For ' + str(q) + ' total cost price is ' + total_cp + ' rupees')
+    print('For ' + str(q) + ' total selling price is ' + total_sp + ' rupees')
+    print('Your total profit made is ' + formula + ' rupees')
 
